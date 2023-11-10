aaa0ea36cefbd73808bbd4e22820627d0fd53fcf
FTCTutus1Tuus <ftc19168@gmail.com>
2023-11-09 21:50:38 -0600

    added traditional auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/DarienOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/DarienOpMode.java
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode.team;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
@@ -21,19 +24,32 @@ public class DarienOpMode extends LinearOpMode {
     public Servo clawLeft;
     public Servo clawRight;
 
-    double clawWristPositionPickup=0.7;
-    double clawWristPositionDrop=0.3;
+    public static double encoderResolution = 537.7 ; //no change unless we change motors
+    public double wheelDiameter = 3.75; // inches
+
+    public double constMult = (wheelDiameter * (Math.PI));
+    public double constant = encoderResolution / constMult;
+    public static double rotationTolerance = 5;
+    public static double power = 0.3;
+    public int encoderPos0;
+    public int encoderPos1;
+    public int encoderPos2;
+    public int encoderPos3;
+    double clawWristPositionPickup = 0.7;
+    double clawWristPositionDrop = 0.3;
+    public static double clawWristPositionGround = 0.6;
     // l open is 0.4 closed is 0.1
     // r open is 0.6 closed is 0.9 for claw servos
-    public double clawLeftPositionOpen=0.4;
-    public double clawLeftPositionClosed=0.08;
-    public double clawRightPositionOpen=0.6;
-    public double clawRightPositionClosed=0.93;
+    public double clawLeftPositionOpen = 0.4;
+    public double clawLeftPositionClosed = 0.08;
+    public double clawRightPositionOpen = 0.6;
+    public double clawRightPositionClosed = 0.93;
 
     public static int armOutPosition = 750;
 
-    public double[] direction = {0.0,0.0};
+    public double[] direction = {0.0, 0.0};
     public double rotation;
+    public int encoderPos;
     public double regularDivBy = 1;
     public double turboDivBy = 1;
     public boolean turboBoost;
@@ -46,19 +62,22 @@ public class DarienOpMode extends LinearOpMode {
     public DcMotor leftArm;
     public DcMotor rightArm;
 
+    public IMU imu;
+
     public VisionPortal visionPortal;
     public TeamPropMaskPipeline teamPropMaskPipeline;
     public AprilTagProcessor aprilTag;
 
     @Override
-    public void runOpMode() throws InterruptedException {}
+    public void runOpMode() throws InterruptedException {
+    }
 
-    public void runIntakeSystem(){
+    public void runIntakeSystem() {
         if (gamepad1.right_bumper) {
             // Load pixels
             leftIntake.setPower(1);
             rightIntake.setPower(-1);
-        } else if(gamepad1.right_trigger>0) {
+        } else if (gamepad1.right_trigger > 0) {
             // Eject pixels
             leftIntake.setPower(-1);
             rightIntake.setPower(1);
@@ -68,11 +87,12 @@ public class DarienOpMode extends LinearOpMode {
             rightIntake.setPower(0);
         }
     }
-    public void runFeederSystem(){
+
+    public void runFeederSystem() {
         if (gamepad1.left_bumper) {
             // Load pixels
             feeder.setPower(-1);
-        } else if (gamepad1.left_trigger>0){
+        } else if (gamepad1.left_trigger > 0) {
             // Eject pixels
             feeder.setPower(1);
         } else {
@@ -80,47 +100,51 @@ public class DarienOpMode extends LinearOpMode {
             feeder.setPower(0);
         }
     }
-    public void runWristSystem(){
+
+    public void runWristSystem() {
         // Wrist to be on fixed positions for grabbing and for dropping on the backboard.
-        if(gamepad2.right_trigger>0) {
+        if (gamepad2.right_trigger > 0) {
             // Pick up pixels from the robot tray.
             setWristPosition("pickup");
-        } else {
-            // do nothing
         }
-        if (gamepad2.left_trigger>0){
+        if (gamepad2.left_trigger > 0) {
             // Drop pixels on backboard
             setWristPosition("drop");
-        } else {
-            // do nothing
+        }
+        if (gamepad2.x) {
+            setWristPosition("dropGround");
         }
         //clawWrist.setPower(gamepad2.left_stick_y/3); // clawWrist control as a continuous servo (CRServo).
     }
-    public void setWristPosition( String position){
-        switch( position ){
+
+    public void setWristPosition(String position) {
+        switch (position) {
             case "pickup":
                 clawWrist.setPosition(clawWristPositionPickup);
                 break;
             case "drop":
                 clawWrist.setPosition(clawWristPositionDrop);
                 break;
+            case "dropGround":
+                clawWrist.setPosition(clawWristPositionGround);
             default:
                 // do nothing;
         }
     }
 
-    public void runClawSystem(){
-        if(gamepad2.right_bumper) {
+    public void runClawSystem() {
+        if (gamepad2.right_bumper) {
             setClawPosition("open");
             // Open the claw
-        } else if (gamepad2.left_bumper){
+        } else if (gamepad2.left_bumper) {
             setClawPosition("closed");
             // Close the claw
         }
 
     }
-    public void setClawPosition( String position){
-        switch( position ){
+
+    public void setClawPosition(String position) {
+        switch (position) {
             case "open":
                 clawLeft.setPosition(clawLeftPositionOpen);
                 clawRight.setPosition(clawRightPositionOpen);
@@ -134,7 +158,7 @@ public class DarienOpMode extends LinearOpMode {
         }
     }
 
-    public void runArmSystem(){
+    public void runArmSystem() {
         // DONE: Arm control on the left joystick up/down.
         leftArm.setPower(-gamepad2.left_stick_y);
         rightArm.setPower(-gamepad2.left_stick_y);
@@ -142,10 +166,10 @@ public class DarienOpMode extends LinearOpMode {
         //rightArm.setPower(gamepad2.right_trigger-gamepad2.left_trigger);
     }
 
-    public void setArmPosition(String position){
+    public void setArmPosition(String position) {
         rightArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         leftArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        switch (position){
+        switch (position) {
             case "out":
                 rightArm.setTargetPosition(armOutPosition);
                 leftArm.setTargetPosition(armOutPosition);
@@ -157,9 +181,10 @@ public class DarienOpMode extends LinearOpMode {
         leftArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
     }
-    public void driveArm( String position ){
+
+    public void driveArm(String position) {
         double power = 0.8;
-        switch( position ){
+        switch (position) {
             case "in":
                 // TODO: power the arm down until it reaches the stopping position. Then turn off the power.
                 leftArm.setPower(-power);
@@ -174,7 +199,7 @@ public class DarienOpMode extends LinearOpMode {
         }
     }
 
-    public void runDriveSystem(){
+    public void runDriveSystem() {
         direction[0] = -gamepad1.left_stick_x;
         direction[1] = -gamepad1.left_stick_y;
         rotation = -gamepad1.right_stick_x;
@@ -182,15 +207,16 @@ public class DarienOpMode extends LinearOpMode {
 
         MoveRobot(direction, -rotation, turboBoost);
     }
-    public void runMacro( String macro ){
-        switch( macro ) {
+
+    public void runMacro(String macro) {
+        switch (macro) {
             case "ReadyToPickup":
                 if (gamepad2.y) {
-                    if (clawWrist.getPosition() < clawWristPositionPickup){
+                    if (clawWrist.getPosition() < clawWristPositionPickup) {
                         // If the wrist is already in PICKUP position, do nothing.
                         setWristPosition("pickup");
                     }
-                    if( clawLeft.getPosition() < clawLeftPositionOpen){
+                    if (clawLeft.getPosition() < clawLeftPositionOpen) {
                         // If the claw is already in OPEN position, do nothing.
                         setClawPosition("open");
                     }
@@ -201,7 +227,7 @@ public class DarienOpMode extends LinearOpMode {
                 break;
             case "ReadyToDrop":
                 if (gamepad2.a) {
-                    if (clawWrist.getPosition() > clawWristPositionDrop){
+                    if (clawWrist.getPosition() > clawWristPositionDrop) {
                         // Only raise the wrist if it's not already in position.
                         setWristPosition("drop");
                     }
@@ -216,7 +242,7 @@ public class DarienOpMode extends LinearOpMode {
         }
     }
 
-    public void MoveRobot(double[] direction, double rotation, boolean turboBoost){
+    public void MoveRobot(double[] direction, double rotation, boolean turboBoost) {
 
         double divBy;
         double wheel0 = clamp(-direction[0] + direction[1] + rotation, -1, 1);
@@ -225,46 +251,56 @@ public class DarienOpMode extends LinearOpMode {
         double wheel3 = clamp(direction[0] + -direction[1] + rotation, -1, 1);
         if (turboBoost) {
             divBy = turboDivBy;
-        }
-        else {
+        } else {
             divBy = regularDivBy;
 
         }
-        telemetry.addData("", wheel0/divBy);
+        telemetry.addData("", wheel0 / divBy);
 
-        MoveMotor(omniMotor0,wheel0/divBy);
-        MoveMotor(omniMotor1,wheel1/divBy);
-        MoveMotor(omniMotor2,wheel2/divBy);
-        MoveMotor(omniMotor3,wheel3/divBy);
+        MoveMotor(omniMotor0, wheel0 / divBy);
+        MoveMotor(omniMotor1, wheel1 / divBy);
+        MoveMotor(omniMotor2, wheel2 / divBy);
+        MoveMotor(omniMotor3, wheel3 / divBy);
     }
 
 
     public void initCamera(boolean isBlue) {
-            // true = blue false = red
-            teamPropMaskPipeline = new TeamPropMaskPipeline(isBlue);
-            // Create the AprilTag processor.
-            aprilTag = new AprilTagProcessor.Builder().build();
+        // true = blue false = red
+        teamPropMaskPipeline = new TeamPropMaskPipeline(isBlue);
+        // Create the AprilTag processor.
+        aprilTag = new AprilTagProcessor.Builder().build();
 
-            // Create the vision portal by using a builder.
-            VisionPortal.Builder builder = new VisionPortal.Builder();
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
 
-            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
 
-            // Set and enable the processor.
-            builder.addProcessor(aprilTag);
-            builder.addProcessor(teamPropMaskPipeline);
+        // Set and enable the processor.
+        builder.addProcessor(aprilTag);
+        builder.addProcessor(teamPropMaskPipeline);
 
-            // Build the Vision Portal, using the above settings.
-            visionPortal = builder.build();
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
 
-            // Disable or re-enable the aprilTag processor at any time.
-            //visionPortal.setProcessorEnabled(aprilTag, true);
+        // Disable or re-enable the aprilTag processor at any time.
+        //visionPortal.setProcessorEnabled(aprilTag, true);
 
-        }   // end method initAprilTag()
+    }   // end method initAprilTag()
 
 
     public void initControls(boolean isAuto) {
         //isAuto: true=auto false=teleop
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(
+                new IMU.Parameters(
+                        new RevHubOrientationOnRobot(
+                                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+                                RevHubOrientationOnRobot.UsbFacingDirection.UP
+                        )
+                )
+        );
+        imu.resetYaw();
+
         omniMotor0 = initializeMotor("omniMotor0");
         omniMotor1 = initializeMotor("omniMotor3");
         omniMotor2 = initializeMotor("omniMotor1");
@@ -295,20 +331,163 @@ public class DarienOpMode extends LinearOpMode {
 
         feeder = hardwareMap.get(CRServo.class, "feeder");
     }
-    public DcMotor initializeMotor(String name){
-         /*This is just a handy dandy function which saves a few lines and looks cool,
-         it initializes the motor and it also initializers the motor power logs for this motor*/
-        DcMotor motor = hardwareMap.get(DcMotor.class, name);
-        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        return motor;
+
+    public void MoveY(int y, double power) {
+
+        resetEncoder();
+        encoderPos = (int) Math.floor((y * constant + 0.5));
+        setTargetPosY();
+        print("moving y",encoderPos);
+        setRunMode();
+
+        setPower(power);
     }
 
-    public void MoveMotor(DcMotor motor, double power){
+
+    public void MoveX(int x, double power) {
+        resetEncoder();
+
+        encoderPos = (int) Math.floor((x * constant * 1.111111) + 0.5);
+        print("moving x",encoderPos);
+        setTargetPosX();
+        setRunMode();
+        setPower(power);
+    }
+
+    public void AutoRotate(double TargetPosDegrees, double power, int direction) {
+        setToRotateRunMode();
+        double initError = Math.abs(TargetPosDegrees-getRawHeading());
+        double truePower;
+        double error;
+        double scaleConstant = initError;
+        boolean isRotating = true;
+        //TODO fix rotation: speed lowers too fast
+        while (isRotating) {
+            error = Math.abs(TargetPosDegrees-getRawHeading());
+            truePower = Math.min(Math.pow((error/scaleConstant),2),1);
+            telemetry.addData("heading ", getRawHeading());
+            telemetry.addData("error ", error);
+            print("", truePower*power);
+
+            setRotatePower(truePower*power, direction);
+            if (error<rotationTolerance) {
+                isRotating = false;
+                setRotatePower(0,0);
+                resetEncoder();
+                return;
+
+            }
+
+        }
+    }
+        public double sigmoid(double x) {
+        //takes in any x value returns from (0,0) to (1,1) scale x accordingly
+        return (1/(1+Math.pow(2.71,(-8*x+4))));
+        }
+        public double getVoltage () {
+            return (hardwareMap.voltageSensor.iterator().next().getVoltage());
+        }
+
+
+        public void setTargetPosY ()
+        {
+
+            omniMotor0.setTargetPosition(-encoderPos);
+            omniMotor1.setTargetPosition(-encoderPos);
+            omniMotor2.setTargetPosition(encoderPos);
+            omniMotor3.setTargetPosition(encoderPos);
+
+        }
+
+        public void setTargetPosX ()
+        {
+
+            omniMotor0.setTargetPosition(-encoderPos);
+            omniMotor1.setTargetPosition(encoderPos);
+            omniMotor2.setTargetPosition(-encoderPos);
+            omniMotor3.setTargetPosition(encoderPos);
+
+        }
+
+
+
+        public void setRunMode ()
+        {
+            omniMotor0.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            omniMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            omniMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            omniMotor3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+
+        public void setToRotateRunMode () {
+            omniMotor0.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            omniMotor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            omniMotor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            omniMotor3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+        public void setPower ( double power)
+        {
+            omniMotor0.setPower(relativePower(power));
+            omniMotor1.setPower(relativePower(power));
+            omniMotor2.setPower(relativePower(power));
+            omniMotor3.setPower(relativePower(power));
+
+        }
+
+        public void setRotatePower ( double power, double direction){
+            omniMotor0.setPower(relativePower(direction * power));
+            omniMotor1.setPower(relativePower(-direction * power));
+            omniMotor2.setPower(relativePower(-direction * power));
+            omniMotor3.setPower(relativePower(direction * power));
+
+        }
+
+        public void resetEncoder ()
+        {
+            omniMotor0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            omniMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            omniMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            omniMotor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+
+        public void waitForMotors ()
+        {
+            while (omniMotor0.isBusy() && omniMotor1.isBusy() && omniMotor2.isBusy() && omniMotor3.isBusy()) {
+            }
+        }
+
+        int move_to_position;
+        double y;
+
+        public void print (String Name, Object message)
+        {
+            telemetry.addData(Name, message);
+            telemetry.update();
+        }
+        public double relativePower ( double intended_power)
+        {
+            return (13 * intended_power) / getVoltage();
+        }
+
+
+        public double getRawHeading () {
+            return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        }
+
+        public DcMotor initializeMotor (String name){
+         /*This is just a handy dandy function which saves a few lines and looks cool,
+         it initializes the motor and it also initializers the motor power logs for this motor*/
+            DcMotor motor = hardwareMap.get(DcMotor.class, name);
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            return motor;
+        }
+
+        public void MoveMotor (DcMotor motor,double power){
          /*This function just moves the motors and updates the
          logs for replay*/
-        motor.setPower(power);
+            motor.setPower(power);
+        }
+        public static double clamp ( double val, double min, double max){
+            return Math.max(min, Math.min(max, val));
+        }
     }
-    public static double clamp(double val, double min, double max) {
-        return Math.max(min, Math.min(max, val));
-    }
-}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/encoderValTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.team;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp
+public class encoderValTest extends DarienOpMode {
+    public void runOpMode() {
+        initControls(true);
+
+        waitForStart();
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                AutoRotate(90, power, -1);
+            } else if (gamepad1.b) {
+                MoveX(5, power);
+            } else if (gamepad1.x) {
+                MoveY(5, power);
+            }
+        }
+    }
+}
